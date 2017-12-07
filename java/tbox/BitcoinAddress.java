39028e2ddf612e765ad47e997b7223a5818a96c5
bitsanity <brydiver@gmail.com>
2017-12-06 17:35:36 -0800

    .

---
+++ java/tbox/BitcoinAddress.java
@@ -0,0 +1,48 @@
+package tbox;
+
+// en.bitcoin.it/wiki/Technical_background_of_version_1_Bitcoin_addresses
+public class BitcoinAddress
+{
+  public BitcoinAddress( byte[] pvkey ) throws Exception
+  {
+    // WARNING:
+    // curve must be configured to use the uncompressed public key format
+    Secp256k1 curve = new Secp256k1();
+
+    if ( !curve.privateKeyIsValid(pvkey) )
+      throw new Exception( "BitcoinAddress(): bad key" );
+
+    // library prepended the 0x04 byte so dont have to do it here
+    byte[] pubkey = curve.publicKeyCreate( pvkey );
+    byte[] hashed = SHA256.hash( pubkey );
+    byte[] riped = RIPEMD160.digest( hashed );
+
+    // prepend the 0x00 for MAIN bitcoin network
+    byte[] riped2 = ByteOps.prepend( MAIN, riped );
+
+    addressB58Check_ = Base58Check.encode( riped2 );
+  }
+
+  public String toString()
+  {
+    return addressB58Check_;
+  }
+
+  private String addressB58Check_ = null;
+
+  private static final byte MAIN = (byte)0x00; // Main network
+
+  // Test --------------------------------------------------------------------
+  public static void main( String[] args ) throws Exception
+  {
+    String rawStr =
+      "18E14A7B6A307F426A94F8114701E7C8E774E7F9A47E2C2035DB29A206321725";
+
+    String pubAdd = "16UwLL9Risc3QfPqBUvKofHmBQ7wMtjvM";
+
+    BitcoinAddress ba = new BitcoinAddress( HexString.decode(rawStr) );
+
+    if ( !ba.publicAddress().equals(pubAdd) )
+      throw new Exception( "BitcoinAddress.main(): FAIL ba: " + ba.toString() );
+  }
+}
